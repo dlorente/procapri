0e71b7fbce70b9f63d478a68623828516315d520
David Lorente <sr.lorente@gmail.com>
2022-11-16 01:24:00 -0300

    Nome do site

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Login - SB Admin</title>
+        <title>PROCAPRI - Programa de Controle Produtivo e Reprodutivo de Caprinos</title>
         <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
     </head>
